e0a8e995091671e8e3ff9b989c99453e512c7406
riadsarkar45 <riadsarkar45@gmail.com>
2024-02-09 11:58:51 +0600

    inline editing functional

--- src/Components/Contents/Tasks.jsx
+++ src/Components/Contents/Tasks.jsx
@@ -1,17 +1,17 @@
 import EditIcon from '@mui/icons-material/Edit';
+import { useState } from 'react';
 
-const Tasks = ({ task, index, editTask, test }) => {
+const Tasks = ({ task, index, editTask, test, handleEdit, handleOnClickUpdate }) => {
     const { taskName } = task;
-    if (test) {
-        console.log('match')
-    }
+
     return (
         <div>
-            <div className={`{bg-gray-500 bg-opacity-20 ${test ? 'bg-red-500' : ''} mt-2 text-2xl p-2 rounded-md flex justify-between items-center}`}>
+            <div className={`{bg-gray-500 bg-opacity-20 ${test ? 'bg-red-500' : 'bg-gray-500'} mt-2 text-2xl p-2 rounded-md flex justify-between items-center}`}>
                 <div>
                     {
                         test ? (
-                            <input className='p-2' type="text" value={taskName} />
+                            <input onChange={(e) => handleEdit(e.target.value)} className='p-2' type="text" placeholder={taskName} />
+
                         ) : (
                             <p>{taskName} </p>
                         )
@@ -20,7 +20,7 @@ const Tasks = ({ task, index, editTask, test }) => {
                 <div>
                     {
                         test ? (
-                            <h2 className='cursor-pointer bg-green-300 p-2 rounded-sm' onClick={() => editTask(index)}>Save</h2>
+                            <h2 className='cursor-pointer bg-green-300 p-2 rounded-sm' onClick={() => handleOnClickUpdate()}>Save</h2>
                         ) : (
                             <h2 className='cursor-pointer' onClick={() => editTask(index)}>< EditIcon /></h2>
                         )
